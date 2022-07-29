3020bee389062cb66c14ff601df126784d9478bf
Florian Schumacher <FloCo12@gmx.net>
2022-07-29 10:35:08 +0200

    Added a function to get a memory map from BIOS.

--- UtilityOS/src/boot/boot.asm
+++ UtilityOS/src/boot/boot.asm
@@ -2,6 +2,7 @@
 [org 0x7C00]
 
 SECTION2 equ 0x7E00
+MMAP equ 0x8000
 
 _start:
 	;store boot disk number
@@ -9,28 +10,27 @@ _start:
 
 	;setup data segment registers
 	mov ax, 0
-	mov ds, ax
 	mov es, ax
-	mov fs, ax
-	mov gs, ax
+	mov ds, ax
 	mov ss, ax
 
 	;setup stack
-	mov sp, 0x7BFF
-	mov bp, sp
+	mov bp, 0x7BFE
+	mov sp, bp
 
 	;load extra segent
+	cld
 	mov ah, 0x02
 	mov al, 1
 	mov ch, 0
-	mov cl, 1
+	mov cl, 2
 	mov dh, 0
 	mov dl, [BootDisk]
 	mov bx, SECTION2
 	int 0x13
 	jc .error
 	
-	;check if CPUID is supported
+	;check if CPUID is supported, if not error
 	pushfd
 	pushfd
 	xor DWORD [esp], 0x00200000
@@ -41,6 +41,10 @@ _start:
 	popfd
 	and eax, 0x00200000
 	je .error
+
+	push WORD MMAP
+	call MakeMemoryMap
+	jc .error
 
 	jmp $
 
@@ -72,5 +76,66 @@ ErrorString: db "Bootloader error!", 13, 10, 0
 times 510-($-$$) db 0
 dw 0xAA55
 
+MakeMemoryMap:
+	push bp
+	mov bp, sp
+	sub sp, 4			;alloc 4 bytes on stack
+
+	mov di, [bp+4]		;move first argument to DI
+	add di, 4
+
+	mov ebx, 0
+	mov edx, 0x0534D4150
+	mov eax, 0xE820
+	mov [es:di+20], DWORD 1
+	mov ecx, 24
+	int 0x15
+	jc .failed
+	mov edx, 0x0534D4150
+	cmp eax, edx
+	jne .failed
+	test ebx, ebx
+	je .failed
+	jmp .start
+	
+	.loop:
+	mov eax, 0xE820
+	mov [es:di+20], DWORD 1
+	mov ecx, 24
+	int 0x15
+	jc .done
+	mov edx, 0x0534D4150
+
+	.start:
+	jcxz .skip
+	cmp cl, 20
+	jbe .notext
+	test BYTE [es:di+20], 1
+	je .skip
+
+	.notext:
+	mov ecx, [es:di+8]
+	or ecx, [es:di+12]
+	jz .skip
+	inc DWORD [bp-4]
+	add di, 24
+
+	.skip:
+	test ebx, ebx
+	jne .loop
+
+	.done:
+	mov eax, [bp-4]
+	mov [bp+4], eax
+	clc
+	jmp .end
+
+	.failed:
+	stc
+
+	.end:
+	mov sp, bp
+	pop bp
+	ret 2
 
 times 1024-($-$$) db 0
