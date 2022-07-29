b7f1d8dd82e99dc3f9730a4ce849a44d0713f486
Florian Schumacher <FloCo12@gmx.net>
2022-07-29 08:18:19 +0200

    Removed function macros, and added code to check for CPUID feature.

--- UtilityOS/src/boot/boot.asm
+++ UtilityOS/src/boot/boot.asm
@@ -1,35 +1,13 @@
-%macro FUNC 1
-%1:
-	push bp,
-	mov bp, sp
-%endmacro
-
-%macro END 1
-	pop bp
-	ret (%1*2)
-%endmacro
-
-%macro ldarg 2
-	mov %1, [bp+(4+(2*%2))]
-%endmacro
-
-%macro INVOKE 1-*
-	%rep %0-1
-		%rotate -1
-		push %1
-	%endrep
-	%rotate -1
-	call %1
-%endmacro
-
 [bits 16]
 [org 0x7C00]
 
 SECTION2 equ 0x7E00
 
 _start:
+	;store boot disk number
 	mov [BootDisk], dl
 
+	;setup data segment registers
 	mov ax, 0
 	mov ds, ax
 	mov es, ax
@@ -37,9 +15,11 @@ _start:
 	mov gs, ax
 	mov ss, ax
 
+	;setup stack
 	mov sp, 0x7BFF
 	mov bp, sp
-	
+
+	;load extra segent
 	mov ah, 0x02
 	mov al, 1
 	mov ch, 0
@@ -48,13 +28,31 @@ _start:
 	mov dl, [BootDisk]
 	mov bx, SECTION2
 	int 0x13
+	jc .error
+	
+	;check if CPUID is supported
+	pushfd
+	pushfd
+	xor DWORD [esp], 0x00200000
+	popfd
+	pushfd
+	pop eax
+	xor eax, [esp]
+	popfd
+	and eax, 0x00200000
+	je .error
 
-	.error:
-	INVOKE Print, ErrorString
 	jmp $
 
-FUNC Print
-	ldarg bx, 0
+	.error:
+	push WORD ErrorString
+	call Print
+	jmp $
+	
+Print:
+	push bp
+	mov bp, sp
+	mov bx, [bp+4]
 	mov ah, 0x0E
 	.loop:
 	mov al, [bx]
@@ -64,17 +62,15 @@ FUNC Print
 	int 0x10
 	jmp .loop
 	.end:
-END 1
+	mov sp, bp
+	pop bp
+	ret 2
 
-BootDisk: db 0
-ErrorString: db "Critical Error!!!", 13, 10, 0
+BootDisk: resb 1
+ErrorString: db "Bootloader error!", 13, 10, 0
 
 times 510-($-$$) db 0
 dw 0xAA55
 
-[bits 16]
-FUNC CheckCPUID
-	
-END 0
 
 times 1024-($-$$) db 0
